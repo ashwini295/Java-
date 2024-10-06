4027703ef282f6ac44ea93e54ed714c7ebc5817d
Ashwini295 <ashwini.h209@gmail.com>
2024-10-06 20:39:21 +0530

    commiting 1st code

---
+++ src/Octex_02102014/Lab001.java
@@ -0,0 +1,11 @@
+package Octex_02102014;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("HelloWord!!!!!!");
+        System.out.println("Ashwini");
+
+    }
+}
